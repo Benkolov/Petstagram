38cf715bc67e44746f76448d02324f399f6b415f
Benkolov <111705203+Benkolov@users.noreply.github.com>
2023-05-30 19:36:48 +0300

    Initial commit

--- Petstagram/settings.py
+++ Petstagram/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)u^)i1!fk_xuh2h_q5meqw#g0$nb-g3e=4_o(7#)17*08na-dj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -114,10 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    # old os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Default primary key field type
